1c5637a02c7d0644a7484908363f8ce4b01e3749
my_jabz <kemika.t@ku.th>
2018-05-14 00:24:09 +0700

    edit css of student page

--- resources/views/manageStudents/index.blade.php
+++ resources/views/manageStudents/index.blade.php
@@ -1,5 +1,5 @@
-<!-- <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> -->
-<!-- <link href="{{ asset('bootstrap/css/studentCSS.css') }}" rel="stylesheet"> -->
+<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
@@ -8,23 +8,19 @@
 <!-- {{ $students }} -->
 
 <!-- add later -->
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
 <script src="//code.jquery.com/jquery-1.12.3.js"></script>
 <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-  <!--  -->
 
-
-
+<link href="{{ asset('bootstrap/css/studentCSS.css') }}" rel="stylesheet">
 
 
 <h1> Manage Students</h1>
 <div class="row">
   <div class="col-1"></div>
-  <div class="col col-xl-10">
+  <div class="col-8">
     <table class="table table-hover" id="table">
       <thead>
         <tr>
@@ -62,6 +58,7 @@
     </table>
   </div>
 </div>
+
 
 
 <div class="row">
