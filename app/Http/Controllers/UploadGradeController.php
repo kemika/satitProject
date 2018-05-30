1907a769df6e62bbfbf75741a25846d86c2f1061
my_jabz <kemika.t@ku.th>
2018-05-30 23:24:52 +0700

    try to join table

--- app/Http/Controllers/UploadGradeController.php
+++ app/Http/Controllers/UploadGradeController.php
@@ -9,18 +9,28 @@ use Excel;
 class UploadGradeController extends Controller
 {
   public function index(){
+    $subjects = Subject::all();
+    $teachings = Teaching::all();
+    $gpas  = GPA::all();
+    $teacher_number = Auth::user()->teacher_number;
+
+    subject_details = Subject::where('teacher_number')
+
     return view('uploadGrade.index');
   }
-    public function exportExcel($type)
-    {
-      Excel::create('Laravel Excel', function($excel) {
 
-        $excel->sheet('Excel sheet', function($sheet) {
+  public function exportExcel($type)
+  {
+    Excel::create('Laravel Excel', function($excel) {
 
-        $sheet->setOrientation('landscape');
+      $excel->sheet('Excel sheet', function($sheet) {
+
+      $sheet->setOrientation('landscape');
 
       });
 
     })->export($type);
-    }
+  }
+
+
 }
