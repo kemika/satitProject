1c05209595286098a3415cfca2dc3e70e92ce7e1
my_jabz <kemika.t@ku.th>
2018-06-03 03:15:01 +0700

    edit css viewGrade, Std, Teacher pages

--- resources/views/grade/index.blade.php
+++ resources/views/grade/index.blade.php
@@ -1,47 +1,145 @@
-@extends('layouts.web')
+<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.12.3.js"></script>
+<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+<link href="{{ asset('css/viewGradeCSS.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="/css/nav.css">
 
 
-@push('script')
-<script type="text/javascript">
-document.getElementById('grade').classList.add('active')
+
+
+
+<head>
+  <div id='cssmenu'>
+  <ul>
+     <li ><a href='/main'>SatitKaset</a></li>
+     <li><a href='/manageStudents'>Manage Student</a></li>
+     <li class='active'><a href='/viewGrade'>Grade</a></li>
+     <li><a href='#'>About</a></li>
+     <li style="float:right">        <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                 {{ __('Logout') }}
+             </a>
+
+             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                 @csrf
+             </form></li>
+
+             <li style="float:right"><a href='#'>{{ auth::user()->firstname.' '.auth::user()->lastname}}</a></li>
+  </ul>
+
+  </div>
+
+</head>
+
+<script>
+  $(document).ready(function() {
+    $('#table').DataTable();
+    $("#dataSubj").hide();
+   $("#dataStd").hide();
+  } );
+
+  $(document).ready(function(){
+    $("#tagSubj").click(function(){
+        $("#dataSubj").show('1000');
+       $("#dataStd").hide('1000');
+
+    });
+
+  });
+
+  $(document).ready(function(){
+    $("#tagStd").click(function(){
+        $("#dataStd").show('1000');
+       $("#dataSubj").hide('1000');
+
+    });
+
+  });
 
 </script>
 
 
-
-@endpush
-
-
-@section('content')
-<h1>View Grade</h1>
-<a href="#table" onclick="document.getElementById('my').style= 'block'">Subjects</a>
+<h1> View Grades</h1>
+<a href="#" id="tagSubj">Subjects</a>
 <br><br>
-<a href="#">Students</a>
+<a href="#" id="tagStd">Students</a>
+
+<center>
+  <div id="dataSubj" style="display">
+    <div class="row" style="width: 100rem; margin-top: -50px;">
+
+      <table class="table table-hover" id="table" style="width: 100rem;">
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Subject ID</th>
+            <th scope="col">Subject name</th>
+            <th scope="col">Action</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php $c=0; ?>
+          @foreach ($subjects as $subject)
+          <?php $c+=1 ?>
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $subject->code }}</td>
+            <td>{{ $subject->name }}</td>
+            <td><button type="button" class="btn btn-primary" data-toggle='modal' data-target='#myModal'>View</button></td>
+          </tr>
+
+          <!-- Modal -->
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body">
+                  <p>This is a large modal.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
 
 
-<div id="my" style="display: none">
+  <div id="dataStd">
+    <div class="row" style="width: 100rem; margin-top: -50px;">
 
-<table class="table table-hover" id="table" style="width: 120rem;">
-  <thead>
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Subject ID</th>
-      <th scope="col">Subject name</th>
+      <table class="table table-hover" id="table" style="width: 100rem;">
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Student ID</th>
+            <th scope="col">Student name</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
 
-    </tr>
-  </thead>
+        <tbody>
 
-  <tbody>
 
-    @foreach ($subjects as $subject)
-    <tr>
-      <td>{{ $loop->iteration }}</td>
-      <td>{{ $subject->subj_number }}</td>
-      <td>{{ $subject->name }}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-</div>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
-@endsection
+</center>
