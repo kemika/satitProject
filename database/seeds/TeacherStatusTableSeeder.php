a9997e214eed1ea60cf91e258291e30eb6ebedcb
my_jabz <kemika.t@ku.th>
2018-07-08 21:17:46 +0700

    edit Database(My)

--- database/seeds/TeacherStatusTableSeeder.php
+++ database/seeds/TeacherStatusTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class StudentsTableSeeder extends Seeder
+class TeacherStatusTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class StudentsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Student::class, 15)->create();
+        factory(App\Teacher_Status::class, 10)->create();
     }
 }
