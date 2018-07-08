b5fea17e9532cc2db541379ac6b6217a074497d9
kemika <kemika.t@ku.th>
2018-07-08 21:24:00 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,21 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         Eloquent::unguard();
-<<<<<<< HEAD
         $this->call(TeacherStatusTableSeeder::class);
-=======
-    //     $this->call(UsersTablesSeeder::class);
-    //     $this->call(StudentsTableSeeder::class);
-    //     $this->call(TeachersTableSeeder::class);
-    //     $this->call(GPAsTableSeeder::class);
-    //     $this->call(SubjectsTableSeeder::class);
-    //     $this->call(TeachingsTableSeeder::class);
-    //     $this->call(AssingnGradesTableSeeder::class);
-    //     $this->call(RoomsTableSeeder::class);
-    //     $this->call(CurriculumsTableSeeder::class);
-    $this->call(Student_StatusTableSeeder::class);
-    $this->call(StudentsTableSeeder::class);
->>>>>>> cd511a565faf90c450711d7184fea20bbf270216
+        $this->call(Student_StatusTableSeeder::class);
+        $this->call(StudentsTableSeeder::class);
     }
 
 
